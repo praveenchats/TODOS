760ab0d84ff2c2edec1de107d0ed3b143f517ce2
Praveen <sharmapraveen.1dec@gmail.com>
2022-08-27 23:39:26 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h3>This is an about component</h3>
+            <p>They list everything that you have to do, with the most important tasks at the top of the list, and the least important tasks at the bottom. By keeping such a list, you make sure that your tasks are written down all in one place so you don't forget anything important.</p>
+
+            <p>1. Increases productivity⌚</p>
+            <p>2. Improves memory📝</p>
+            <p>3. Reduces stress💪</p>
+            <p>4. Allows for more personal time⏲️</p>
+            <p>5. Reduces anxiety and improves mental health❤️‍🩹</p>
+            <p>6. Provides a sense of accomplishment🔥</p>
+            <p>7. Set deadlines for each goal🥧</p>
+        </div >
+    )
+}
